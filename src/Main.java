fb55eefbae5c9b7523d5e11a4e44e4bfa65e43be
ViniciusOliver13 <nvidianew3201@gmail.com>
2025-07-10 19:53:10 -0300

    feat: Implementa a inicialização do banco de dados e adiciona a pasta DAO

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+import data.ConexaoDoBanco; 
+
 public class Main {
-    
+    public static void main(String[] args) {
+
+        try {
+            ConexaoDoBanco.iniciarBanco();
+            System.out.println("Banco de dados iniciado com sucesso.");
+            
+        } catch (Exception e) {
+            System.err.println("Erro ao iniciar o banco de dados: " + e.getMessage());
+        }
+    }
 }
