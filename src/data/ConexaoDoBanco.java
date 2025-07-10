00c8553ebec8bc66db72e17db51d160c80284baa
thyagofab <thyago.fabricio2015@hotmail.com>
2025-07-09 21:06:51 -0300

    Feat: adiciona dependências do banco e realiza conexão

---
+++ src/data/ConexaoDoBanco.java
@@ -0,0 +1,27 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+//import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConexaoDoBanco {
+    private Connection conexao;
+    private Statement statement;  // usado para executar os comandos sql 
+    
+    ConexaoDoBanco(){
+        try{
+            this.conexao = DriverManager.getConnection("jdbc:sqlite:banco.db"); //Inicializando o banco de dados , caso não exista esse arquivo ai automaticamente é criado peloa SQLlite
+
+            this.statement = conexao.createStatement(); //Inicializando o Statement que vai servi para executar os comandos SQL
+
+            statement.setQueryTimeout(3); //Espera  só por 3 segundos para conectar
+
+
+        }catch(SQLException e){
+            System.out.println("Erro de inicialização!");
+            System.out.println(e);
+        }
+    }
+}
